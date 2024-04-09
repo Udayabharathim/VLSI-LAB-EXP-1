e47b02df01add529da9a25eb3ddfff79f71158d7
Udayabharathim <160568654+Udayabharathim@users.noreply.github.com>
2024-04-09 19:52:07 +0530

    Update Exp1_Halfsubractor.v

--- Exp1_Halfsubractor.v
+++ Exp1_Halfsubractor.v
@@ -1,7 +1,6 @@
-module hs(a,b,difference,borrow);
-input a,b;
-output difference,borrow;
-
-
-
+module hs (a, b, diff, borr); 
+input a,b; 
+output diff, borr; 
+xor gl (diff,a,borr);  
+and g2 (borr, ~a,b); 
 endmodule
